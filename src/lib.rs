4b01563c75fac4511b1fe29dc82c79262a93e876
Félix Saparelli <felix@passcod.name>
2014-12-11 22:14:35 +1300

    Event and Op

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+#![feature(phase)]
+#[phase(plugin, link)] extern crate log;
+
+use std::fmt;
+
 #[test]
 fn it_works() {
+}
+
+pub struct Event {
+  pub name: Path,
+  pub op: Op,
+}
+
+pub enum Op {
+  Create,
+  Write,
+  Remove,
+  Rename,
+  Chmod,
+}
+
+impl fmt::Show for Op {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+    match self {
+      &Op::Create  => "Create",
+      &Op::Write   => "Write",
+      &Op::Remove  => "Remove",
+      &Op::Rename  => "Rename",
+      &Op::Chmod   => "Chmod",
+    }.fmt(f)
+  }
 }
