32faa2ef5b5ef8aef50d5d2efb4cd6212e0c504e
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-05-08 12:36:58 -0700

    remove workarounds; fix made it into rustc

--- src/inotify/mod.rs
+++ src/inotify/mod.rs
@@ -151,10 +151,7 @@ impl Watcher for INotifyWatcher {
   }
 
   fn unwatch(&mut self, path: &Path) -> Result<(), Error> {
-    // FIXME:
-    // once https://github.com/rust-lang/rust/pull/22351 gets merged,
-    // just use a &Path
-    match self.watches.remove(&path.to_path_buf()) {
+    match self.watches.remove(path) {
       None => Err(Error::WatchNotFound),
       Some(p) => {
         let w = &p.0;
