ad5e27424d65b2467e825c1171f48724e2f1d6ea
John Quigley <jmquigs@gmail.com>
2015-11-19 19:01:49 -0500

    Windows: eliminate warning in test

--- tests/notify.rs
+++ tests/notify.rs
@@ -107,6 +107,7 @@ fn validate_watch_single_file<F, W>(ctor: F) where
       // them all in the same directory.
       let mut excluded_file = NamedTempFile::new().unwrap();
       let another_excluded_file = NamedTempFile::new().unwrap();
+      let _ = another_excluded_file; // eliminate warning
       excluded_file.write_all(b"shouldn't get an event for this").unwrap();
 
       file.write_all(b"foo").unwrap();
