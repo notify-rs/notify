a3e66f7830832b586c3fc382c35da938698c7c9b
Daniel Faust <hessijames@gmail.com>
2016-07-09 12:50:11 +0200

    Implement CREATE and DELETE for PollWatcher
    Also implement RecursiveMode for watch(..).

--- src/poll.rs
+++ src/poll.rs
@@ -3,7 +3,7 @@
 //! Checks the `watch`ed paths periodically to detect changes. This implementation only uses
 //! cross-platform APIs; it should function on any platform that the Rust standard library does.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use std::sync::mpsc::Sender;
 use std::fs;
@@ -16,11 +16,12 @@ use self::walkdir::WalkDir;
 use filetime::FileTime;
 
 extern crate walkdir;
+extern crate time;
 
 /// Polling based `Watcher` implementation
 pub struct PollWatcher {
     tx: Sender<Event>,
-    watches: Arc<RwLock<HashSet<PathBuf>>>,
+    watches: Arc<RwLock<HashMap<PathBuf, bool>>>,
     open: Arc<RwLock<bool>>,
 }
 
@@ -29,7 +30,7 @@ impl PollWatcher {
     pub fn with_delay(tx: Sender<Event>, delay: u32) -> Result<PollWatcher> {
         let mut p = PollWatcher {
             tx: tx,
-            watches: Arc::new(RwLock::new(HashSet::new())),
+            watches: Arc::new(RwLock::new(HashMap::new())),
             open: Arc::new(RwLock::new(true)),
         };
         p.run(delay);
@@ -42,32 +43,66 @@ impl PollWatcher {
         let open = self.open.clone();
         thread::spawn(move || {
             // In order of priority:
-            // TODO: populate mtimes before loop, and then handle creation events
-            // TODO: handle deletion events
             // TODO: handle chmod events
             // TODO: handle renames
             // TODO: DRY it up
-            let mut mtimes: HashMap<PathBuf, u64> = HashMap::new();
-            loop {
-                if delay != 0 {
-                    thread::sleep(Duration::from_millis(delay as u64));
+            let mut mtimes: HashMap<PathBuf, (u64, f64)> = HashMap::new();
+
+            let mut current_time = time::precise_time_s();
+
+            for (watch, is_recursive) in watches.read().unwrap().iter() {
+                match fs::metadata(watch) {
+                    Err(e) => {
+                        let _ = tx.send(Event {
+                            path: Some(watch.clone()),
+                            op: Err(Error::Io(e)),
+                        });
+                        continue;
+                    }
+                    Ok(metadata) => {
+                        if !metadata.is_dir() {
+                            let modified = FileTime::from_last_modification_time(&metadata).seconds();
+                            mtimes.insert(watch.clone(), (modified, current_time));
+                        } else {
+                            let depth = if *is_recursive {
+                                usize::max_value()
+                            } else {
+                                1
+                            };
+                            for entry in WalkDir::new(watch)
+                                                .follow_links(true)
+                                                .max_depth(depth)
+                                                .into_iter()
+                                                .filter_map(|e| e.ok()) {
+                                let path = entry.path();
+
+                                match entry.metadata() {
+                                    Err(e) => {
+                                        let _ = tx.send(Event {
+                                            path: Some(path.to_path_buf()),
+                                            op: Err(Error::Io(e.into())),
+                                        });
+                                    }
+                                    Ok(m) => {
+                                        let modified = FileTime::from_last_modification_time(&m).seconds();
+                                        mtimes.insert(path.to_path_buf(), (modified, current_time));
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
+            }
+
+            loop {
                 if !(*open.read().unwrap()) {
                     break;
                 }
 
-                for watch in watches.read().unwrap().iter() {
-                    let meta = fs::metadata(watch);
+                current_time = time::precise_time_s();
 
-                    if !meta.is_ok() {
-                        let _ = tx.send(Event {
-                            path: Some(watch.clone()),
-                            op: Err(Error::PathNotFound),
-                        });
-                        continue;
-                    }
-
-                    match meta {
+                for (watch, is_recursive) in watches.read().unwrap().iter() {
+                    match fs::metadata(watch) {
                         Err(e) => {
                             let _ = tx.send(Event {
                                 path: Some(watch.clone()),
@@ -75,61 +110,93 @@ impl PollWatcher {
                             });
                             continue;
                         }
-                        Ok(stat) => {
-                            let modified = FileTime::from_last_modification_time(&stat).seconds();
-
-                            match mtimes.insert(watch.clone(), modified) {
-                                None => continue, // First run
-                                Some(old) => {
-                                    if modified > old {
-                                        let _ = tx.send(Event {
-                                            path: Some(watch.clone()),
-                                            op: Ok(op::WRITE),
-                                        });
-                                        continue;
+                        Ok(metadata) => {
+                            if !metadata.is_dir() {
+                                let modified = FileTime::from_last_modification_time(&metadata).seconds();
+                                match mtimes.insert(watch.clone(), (modified, current_time)) {
+                                    None => {
+                                        unreachable!();
                                     }
-                                }
-                            }
-
-                            if !stat.is_dir() {
-                                continue;
-                            }
-                        }
-                    }
-
-                    // TODO: more efficient implementation where the dir tree is cached?
-                    for entry in WalkDir::new(watch)
-                                     .follow_links(true)
-                                     .into_iter()
-                                     .filter_map(|e| e.ok()) {
-                        let path = entry.path();
-
-                        match fs::metadata(&path) {
-                            Err(e) => {
-                                let _ = tx.send(Event {
-                                    path: Some(path.to_path_buf()),
-                                    op: Err(Error::Io(e)),
-                                });
-                                continue;
-                            }
-                            Ok(stat) => {
-                                let modified = FileTime::from_last_modification_time(&stat)
-                                                   .seconds();
-                                match mtimes.insert(path.to_path_buf(), modified) {
-                                    None => continue, // First run
-                                    Some(old) => {
-                                        if modified > old {
+                                    Some((old_modified, _)) => {
+                                        if modified > old_modified {
                                             let _ = tx.send(Event {
-                                                path: Some(path.to_path_buf()),
+                                                path: Some(watch.clone()),
                                                 op: Ok(op::WRITE),
                                             });
-                                            continue;
+                                        }
+                                    }
+                                }
+                            } else {
+                                let depth = if *is_recursive {
+                                    usize::max_value()
+                                } else {
+                                    1
+                                };
+                                for entry in WalkDir::new(watch)
+                                                    .follow_links(true)
+                                                    .max_depth(depth)
+                                                    .into_iter()
+                                                    .filter_map(|e| e.ok()) {
+                                    let path = entry.path();
+
+                                    match entry.metadata() {
+                                        Err(e) => {
+                                            let _ = tx.send(Event {
+                                                path: Some(path.to_path_buf()),
+                                                op: Err(Error::Io(e.into())),
+                                            });
+                                        }
+                                        Ok(m) => {
+                                            let modified = FileTime::from_last_modification_time(&m).seconds();
+                                            match mtimes.insert(path.to_path_buf(), (modified, current_time)) {
+                                                None => {
+                                                    let _ = tx.send(Event {
+                                                        path: Some(path.to_path_buf()),
+                                                        op: Ok(op::CREATE),
+                                                    });
+                                                }
+                                                Some((old_modified, _)) => {
+                                                    if modified > old_modified {
+                                                        let _ = tx.send(Event {
+                                                            path: Some(path.to_path_buf()),
+                                                            op: Ok(op::WRITE),
+                                                        });
+                                                    }
+                                                }
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                }
+
+                let mut removed: Vec<PathBuf> = Vec::new();
+
+                'paths: for (ref path, &(_, last_checked)) in &mtimes {
+                    for (watch, _) in watches.read().unwrap().iter() {
+                        if path.starts_with(watch) {
+                            if last_checked < current_time {
+                                let _ = tx.send(Event {
+                                    path: Some(path.to_path_buf()),
+                                    op: Ok(op::REMOVE),
+                                });
+                                removed.push(path.to_path_buf());
+                            }
+                            continue 'paths;
+                        }
+                    }
+                    // not found in watches
+                    removed.push(path.to_path_buf());
+                }
+
+                for path in removed {
+                    mtimes.remove(&path);
+                }
+
+                if delay != 0 {
+                    thread::sleep(Duration::from_millis(delay as u64));
                 }
             }
         });
@@ -142,12 +209,12 @@ impl Watcher for PollWatcher {
     }
 
     fn watch<P: AsRef<Path>>(&mut self, path: P, recursive_mode: RecursiveMode) -> Result<()> {
-        (*self.watches).write().unwrap().insert(path.as_ref().to_path_buf());
+        (*self.watches).write().unwrap().insert(path.as_ref().to_path_buf(), recursive_mode.is_recursive());
         Ok(())
     }
 
     fn unwatch<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
-        if (*self.watches).write().unwrap().remove(path.as_ref()) {
+        if (*self.watches).write().unwrap().remove(path.as_ref()).is_some() {
             Ok(())
         } else {
             Err(Error::WatchNotFound)
--- tests/watcher.rs
+++ tests/watcher.rs
@@ -118,7 +118,18 @@ fn watch_recursive_create_directory() {
 
     fs::File::create(file2.as_path()).expect("failed to create file");
 
-    assert_eq!(recv_events(&rx), vec![
+    let actual = if cfg!(target_os="windows") {
+        // Windows may sneak a write event in there
+        let mut events = recv_events(&rx);
+        events.retain(|&(_, op)| op != op::WRITE);
+        events
+    } else if cfg!(target_os="macos") {
+        inflate_events(recv_events(&rx))
+    } else {
+        recv_events(&rx)
+    };
+
+    assert_eq!(actual, vec![
         (new_dir, op::CREATE),
         (file1, op::CREATE)
     ]);
@@ -402,4 +413,276 @@ fn watch_file() {
             (path1, op::WRITE)
         ]);
     }
+}
+
+#[test]
+fn poll_watch_recursive_create_directory() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let mut new_dir = temp_dir.path().to_owned();
+    new_dir.push("new_dir");
+    let mut file1 = new_dir.clone();
+    file1.push("file1");
+    let mut file2 = new_dir.clone();
+    file2.push("file2");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(temp_dir.path().to_owned(), RecursiveMode::Recursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::create_dir(new_dir.as_path()).expect("failed to create directory");
+    fs::File::create(file1.as_path()).expect("failed to create file");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    watcher.unwatch(temp_dir.path().to_owned()).expect("failed to unwatch directory");
+
+    fs::File::create(file2.as_path()).expect("failed to create file");
+
+    let mut actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    assert_eq!(actual, vec![
+        (temp_dir.into_path(), op::WRITE), // parent directory gets modified
+        (new_dir, op::CREATE),
+        (file1, op::CREATE)
+    ]);
+}
+
+#[test]
+fn poll_watch_recursive_move() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let mut sub_dir1a = temp_dir.path().to_owned();
+    sub_dir1a.push("sub_dir1a");
+    let mut sub_dir1b = temp_dir.path().to_owned();
+    sub_dir1b.push("sub_dir1b");
+    let mut path1a = temp_dir.path().to_owned();
+    path1a.push("sub_dir1a");
+    path1a.push("file1.bin");
+    let mut path1b = temp_dir.path().to_owned();
+    path1b.push("sub_dir1b");
+    path1b.push("file1.bin");
+    let mut path2 = temp_dir.path().to_owned();
+    path2.push("sub_dir1b");
+    path2.push("file2.bin");
+
+    fs::create_dir(sub_dir1a.as_path()).expect("failed to create directory");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(temp_dir.path().to_owned(), RecursiveMode::Recursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::File::create(path1a.as_path()).expect("failed to create file");
+
+    let mut actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    assert_eq!(actual, vec![
+        (sub_dir1a.clone(), op::WRITE), // parent directory gets modified
+        (path1a.clone(), op::CREATE),
+    ]);
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::rename(sub_dir1a.as_path(), sub_dir1b.as_path()).expect("failed to rename file");
+    fs::File::create(path2.as_path()).expect("failed to create file");
+
+    actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    if cfg!(target_os="windows") {
+        assert_eq!(actual, vec![
+            (temp_dir.into_path(), op::WRITE), // parent directory gets modified
+            (sub_dir1a, op::REMOVE),
+            (path1a, op::REMOVE),
+            (sub_dir1b.clone(), op::CREATE),
+            (sub_dir1b, op::WRITE), // parent directory gets modified
+            (path1b, op::CREATE),
+            (path2, op::CREATE),
+        ]);
+    } else {
+        assert_eq!(actual, vec![
+            (temp_dir.into_path(), op::WRITE), // parent directory gets modified
+            (sub_dir1a, op::REMOVE),
+            (path1a, op::REMOVE),
+            (sub_dir1b, op::CREATE),
+            (path1b, op::CREATE),
+            (path2, op::CREATE),
+        ]);
+    }
+}
+
+#[test]
+fn poll_watch_recursive_move_in() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let mut sub_dir1 = temp_dir.path().to_owned();
+    sub_dir1.push("sub_dir1");
+    let mut sub_dir2 = sub_dir1.clone();
+    sub_dir2.push("sub_dir2");
+    let watch_dir = TempDir::new_in(temp_dir.path(), "watch_dir").expect("failed to create temporary directory");
+    let mut sub_dir1a = watch_dir.path().to_owned();
+    sub_dir1a.push("sub_dir1");
+    let mut sub_dir2a = sub_dir1a.clone();
+    sub_dir2a.push("sub_dir2");
+    let mut path = watch_dir.path().to_owned();
+    path.push("sub_dir1");
+    path.push("sub_dir2");
+    path.push("new_file.bin");
+
+    fs::create_dir(sub_dir1.as_path()).expect("failed to create directory");
+    fs::create_dir(sub_dir2.as_path()).expect("failed to create directory");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(watch_dir.path().to_owned(), RecursiveMode::Recursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::rename(sub_dir1.as_path(), sub_dir1a.as_path()).expect("failed to rename file");
+    fs::File::create(path.as_path()).expect("failed to create file");
+
+    let mut actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    if cfg!(target_os="windows") {
+        assert_eq!(actual, vec![
+            (watch_dir.into_path(), op::WRITE), // parent directory gets modified
+            (sub_dir1a, op::CREATE),
+            (sub_dir2a.clone(), op::CREATE),
+            (sub_dir2a, op::WRITE), // extra write event
+            (path, op::CREATE),
+        ]);
+    } else {
+        assert_eq!(actual, vec![
+            (watch_dir.into_path(), op::WRITE), // parent directory gets modified
+            (sub_dir1a, op::CREATE),
+            (sub_dir2a, op::CREATE),
+            (path, op::CREATE),
+        ]);
+    }
+}
+
+#[test]
+fn poll_watch_recursive_move_out() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let watch_dir = TempDir::new_in(temp_dir.path(), "watch_dir").expect("failed to create temporary directory");
+
+    let mut sub_dir1a = watch_dir.path().to_owned();
+    sub_dir1a.push("sub_dir1a");
+    let mut sub_dir1b = temp_dir.path().to_owned();
+    sub_dir1b.push("sub_dir1b");
+    let mut sub_dir2 = sub_dir1a.clone();
+    sub_dir2.push("sub_dir2");
+    let mut path1 = watch_dir.path().to_owned();
+    path1.push("sub_dir1a");
+    path1.push("sub_dir2");
+    path1.push("file1.bin");
+    let mut path2 = temp_dir.path().to_owned();
+    path2.push("sub_dir1b");
+    path2.push("sub_dir2");
+    path2.push("file2.bin");
+
+    fs::create_dir(sub_dir1a.as_path()).expect("failed to create directory");
+    fs::create_dir(sub_dir2.as_path()).expect("failed to create directory");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(watch_dir.path().to_owned(), RecursiveMode::Recursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::File::create(path1.as_path()).expect("failed to create file");
+
+    let mut actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    assert_eq!(actual, vec![
+        (sub_dir2.clone(), op::WRITE), // parent directory gets modified
+        (path1.clone(), op::CREATE),
+    ]);
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::rename(sub_dir1a.as_path(), sub_dir1b.as_path()).expect("failed to rename file");
+    fs::File::create(path2.as_path()).expect("failed to create file");
+
+    actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    assert_eq!(actual, vec![
+        (watch_dir.into_path(), op::WRITE), // parent directory gets modified
+        (sub_dir1a, op::REMOVE),
+        (sub_dir2, op::REMOVE),
+        (path1, op::REMOVE),
+    ]);
+}
+
+#[test]
+fn poll_watch_nonrecursive() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let mut sub_dir1 = temp_dir.path().to_owned();
+    sub_dir1.push("sub_dir1");
+    let mut sub_dir2 = temp_dir.path().to_owned();
+    sub_dir2.push("sub_dir2");
+    let mut file0 = temp_dir.path().to_owned();
+    file0.push("file0.bin");
+    let mut file1 = sub_dir1.clone();
+    file1.push("file1.bin");
+    let mut file2 = sub_dir2.clone();
+    file2.push("file2.bin");
+
+    fs::create_dir(sub_dir1.as_path()).expect("failed to create directory");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(temp_dir.path().to_owned(), RecursiveMode::NonRecursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    fs::create_dir(sub_dir2.as_path()).expect("failed to create directory");
+    fs::File::create(file0.as_path()).expect("failed to create file");
+    fs::File::create(file1.as_path()).expect("failed to create file");
+    fs::File::create(file2.as_path()).expect("failed to create file");
+
+    let mut actual = recv_events(&rx);
+    actual.sort_by(|a, b| a.0.cmp(&b.0));
+
+    assert_eq!(actual, vec![
+        (temp_dir.into_path(), op::WRITE), // parent directory gets modified
+        (file0, op::CREATE),
+        (sub_dir1, op::WRITE), // parent directory gets modified
+        (sub_dir2, op::CREATE),
+    ]);
+}
+
+#[test]
+fn poll_watch_file() {
+    let temp_dir = TempDir::new("temp_dir").expect("failed to create temporary directory");
+    let mut path1 = temp_dir.path().to_owned();
+    path1.push("file1.bin");
+    let mut path2 = temp_dir.path().to_owned();
+    path2.push("file2.bin");
+
+    let mut file1 = fs::File::create(path1.as_path()).expect("failed to create file");
+
+    let (tx, rx) = mpsc::channel();
+    let mut watcher = PollWatcher::with_delay(tx, 100).expect("failed to create recommended watcher");
+    watcher.watch(path1.clone(), RecursiveMode::Recursive).expect("failed to watch directory");
+
+    thread::sleep(Duration::from_millis(1100)); // PollWatcher has only a resolution of 1 second
+
+    file1.write("some data".as_bytes()).expect("failed to write to file");
+    file1.sync_all().expect("failed to sync file");
+    if cfg!(target_os="macos") {
+        drop(file1); // file needs to be closed for fsevent
+    }
+
+    fs::File::create(path2.as_path()).expect("failed to create file");
+
+    assert_eq!(recv_events(&rx), vec![
+        (path1, op::WRITE)
+    ]);
 }
