1f27c10e34d289e968be13f885eff95a8c2aeb46
Félix Saparelli <felix@passcod.name>
2018-04-26 20:56:07 +1200

    Add notes on what the current problems are

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,40 @@ fn main() {
     println!("Acquired event sub: {}", token);
 
     println!("Run");
+    // So basically the problem here... is I'm trying to run the event loop from an event that will
+    // not exist without the inner-spawned future (the backend core) running, so nothing ever runs.
+    // In some applications, that wouldn't necessarily matter because the loop will be driven by
+    // other things anyway e.g. network. But in most applications the Notify event sources will be
+    // the main driving forces. So Notify needs to run from itself.
+    //
+    // This may need more reading/thought, but my main idea is to have two mechanisms:
+    //
+    // 1. An initial kickstart process employing an exponential growth to kick off futures at timed
+    //    intervals starting at 1ms and multiplying by 2 until it gets to 256ms.
+    //
+    // 2. A timer loop that only runs if no event was received within the last 256ms, to keep the
+    //    loop refreshing if needed. This second one might not be needed, tbc.
+    //
+    // To support at least that second one, but also as a useful feature, needs timestamps to be
+    // added to events. Thinking further, while local-filesystem-based backends do not provide this,
+    // more advanced backends, especially remote backends, could provide event timestamps themselves
+    // so having timestamps in the core Event type would be useful.
+    //
+    // The idea there is to have an Option<Timestamp>, using whatever timestamp type is most useful
+    // and general, but also considering usability (because some processing will need to be done in
+    // Notify itself, so an opaque type would be annoying). Backends would have the option to add a
+    // timestamp if they know it, otherwise the Life would add the timestamp in on receipt. If a
+    // timestamp is already present it will not be modified.
+    //
+    // Something that could also be useful for debugging at least is event source. For this I'm
+    // also considering adding a name() -> String method to the trait, so that a backend can express
+    // its name. That would supplant the Life and Selector naming facilities. Event source might
+    // also be interesting for later processing e.g. applying things from events based on provenance
+    // especially in scenarios where events might come from multiple sources simultaneously aka
+    // "partial path set runtime fallback".
+    //
+    // I'm going to sit on this for a day or two to let the idea settle then pick it back later.
+
     // tokio::run(events.for_each(|event| {
     //     println!("Event: {:?}", event);
     //     Ok(())
