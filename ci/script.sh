e3239b359e1a01bbc30d5b50014976f937390e81
Félix Saparelli <felix@passcod.name>
2019-01-05 20:39:38 +1300

    [ci] Actually run tests, maybe?

--- ci/script.sh
+++ ci/script.sh
@@ -13,20 +13,18 @@ bothx() {
   fi
 }
 
-main() {
-  if [ ! -z $DISABLE_TESTS ]; then
-      bothx build
-  else
-    if [ ! -z $NO_CROSS ]; then
-      if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-        TARGET="x86_64-unknown-linux-gnu"
-      elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
-        TARGET="x86_64-apple-darwin"
-      elif [[ "$TRAVIS_OS_NAME" = "windows" ]]; then
-        TARGET="x86_64-pc-windows-msvc"
-      fi
+if [ ! -z $DISABLE_TESTS ]; then
+    bothx build
+else
+  if [ ! -z $NO_CROSS ]; then
+    if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+      TARGET="x86_64-unknown-linux-gnu"
+    elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+      TARGET="x86_64-apple-darwin"
+    elif [[ "$TRAVIS_OS_NAME" = "windows" ]]; then
+      TARGET="x86_64-pc-windows-msvc"
     fi
-
-    bothx test
   fi
-}
+
+  bothx test
+fi
