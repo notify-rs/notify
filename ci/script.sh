4395278926e32607c6ae6a2a40d13e43be6960d2
Félix Saparelli <felix@passcod.name>
2019-01-05 20:03:49 +1300

    [ci] Simplify build script

--- ci/script.sh
+++ ci/script.sh
@@ -3,37 +3,26 @@ set -ex
 bothx() {
   if [ -z $NO_CROSS ]; then
     cross $* --target $TARGET
-    cross $* --target $TARGET --release
   else
     cargo $*
-    cargo $* --release
   fi
 }
 
 main() {
-    bothx build
+  if [ ! -z $DISABLE_TESTS ]; then
+      bothx build
+      return
+  fi
 
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
+  if [ ! -z $NO_CROSS ]; then
+    if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+      TARGET="x86_64-unknown-linux-gnu"
+    elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+      TARGET="x86_64-apple-darwin"
     fi
+  fi
 
-    if [ ! -z $NO_CROSS ]; then
-      if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-        TARGET="x86_64-unknown-linux-gnu"
-      elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
-        TARGET="x86_64-apple-darwin"
-      fi
-    fi
-
-    bothx test
-    bothx test -p notify-backend
-    echo bothx test -p notify-backend-poll-tree
-
-    if [[ "$TARGET" =~ -darwin$ ]]; then
-      echo bothx test -p notify-backend-fsevents
-    elif [[ "$TARGET" =~ -linux- ]]; then
-      bothx test -p notify-backend-inotify
-    fi
+  bothx test
 }
 
 # we don't run the "test phase" when doing deploys
