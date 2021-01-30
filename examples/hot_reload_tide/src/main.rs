d49f46635859b46367e928504b3e7d586e2717a5
Sasho Savov <savovs@gmail.com>
2021-01-30 16:51:56 +0200

    Add comments

--- examples/hot_reload_tide/src/main.rs
+++ examples/hot_reload_tide/src/main.rs
@@ -1,3 +1,8 @@
+
+// Imagine this is a web app that remembers information about audio messages.
+// It has a config.json file that acts as a database,
+// you can edit the configuration and the app will pick up changes without the need to restart it.
+// This concept is known as hot-reloading.
 use hot_reload_tide::messages::{load_config, Config};
 use notify::{
     event::ModifyKind,
@@ -8,13 +13,25 @@ use tide::{Body, Response};
 
 const CONFIG_PATH: &str = "config.json";
 
+// Because we're running a web server we need a runtime,
+// for more information on async runtimes, please check out [async-std](https://github.com/async-rs/async-std)
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let config = load_config(CONFIG_PATH).unwrap();
+
+    // We wrap the data a mutex under an atomic reference counted pointer
+    // to guarantee that the config won't be read and written to at the same time.
+    // To learn about how that works,
+    // please check out the [Fearless Concurrency](https://doc.rust-lang.org/book/ch16-00-concurrency.html) chapter of the Rust book.
     let config = Arc::new(Mutex::new(config));
     let cloned_config = Arc::clone(&config);
 
+    // We listen to file changes by giving Notify
+    // a function that will get called when events happen
     let mut watcher: RecommendedWatcher =
+        // To make sure that the config lives as long as the function
+        // we need to move the ownership of the config inside the function
+        // To learn more about move please read [Using move Closures with Threads](https://doc.rust-lang.org/book/ch16-01-threads.html?highlight=move#using-move-closures-with-threads)
         Watcher::new_immediate(move |result: Result<Event, Error>| {
             let event = result.unwrap();
 
@@ -28,6 +45,7 @@ async fn main() -> tide::Result<()> {
 
     watcher.watch(CONFIG_PATH, RecursiveMode::Recursive)?;
 
+    // We set up a web server using [Tide](https://github.com/http-rs/tide)
     let mut app = tide::with_state(config);
 
     app.at("/messages").get(get_messages);
