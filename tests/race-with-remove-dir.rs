3a889bcae4d9be22d89447b80a26a4692035d1a3
Erick Tryzelaar <etryzelaar@google.com>
2021-05-11 17:31:34 -0700

    Use tempfile to make tempdirs
    This avoids accidentally including non-hermetic data in tests.

--- tests/race-with-remove-dir.rs
+++ tests/race-with-remove-dir.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, thread, time::Duration};
+use std::{fs, thread, time::Duration};
 
 use notify::{immediate_watcher, RecursiveMode, Watcher};
 
@@ -6,11 +6,10 @@ use notify::{immediate_watcher, RecursiveMode, Watcher};
 /// Note: This test will fail if your temp directory is not writable.
 #[test]
 fn test_race_with_remove_dir() {
-    let tmpdir = env::temp_dir().join(".tmprPcUcB");
-    fs::create_dir_all(&tmpdir).unwrap();
+    let tmpdir = tempfile::tempdir().unwrap();
 
     {
-        let tmpdir = tmpdir.clone();
+        let tmpdir = tmpdir.path().to_path_buf();
         thread::spawn(move || {
             let mut watcher = immediate_watcher(move |result| {
                 eprintln!("received event: {:?}", result);
@@ -21,7 +20,7 @@ fn test_race_with_remove_dir() {
         });
     }
 
-    let subdir = tmpdir.join("146d921d.tmp");
+    let subdir = tmpdir.path().join("146d921d.tmp");
     fs::create_dir_all(&subdir).unwrap();
     fs::remove_dir_all(&tmpdir).unwrap();
     thread::sleep(Duration::from_secs(1));
