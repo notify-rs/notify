13f4da823da65980e16535d720f4d5884e555b59
Aron Heinecke <aron.heinecke@t-online.de>
2021-07-22 17:08:35 +0200

    Prepare 5.0.0-pre.11
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! notify = "5.0.0-pre.10"
+//! notify = "5.0.0-pre.11"
 //! ```
 //!
 //! ## Serde
@@ -12,7 +12,7 @@
 //! Events are serialisable via [serde] if the `serde` feature is enabled:
 //!
 //! ```toml
-//! notify = { version = "5.0.0-pre.10", features = ["serde"] }
+//! notify = { version = "5.0.0-pre.11", features = ["serde"] }
 //! ```
 //!
 //! [serde]: https://serde.rs
