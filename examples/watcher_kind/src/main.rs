4c169bfb9bb5da6449388b9fc90e70932af1388c
Travis Finkenauer <tmfinken@gmail.com>
2022-01-06 19:57:04 -0800

    Format with `cargo fmt`

--- examples/watcher_kind/src/main.rs
+++ examples/watcher_kind/src/main.rs
@@ -4,7 +4,7 @@ use notify::*;
 fn main() {
     let (tx, rx) = std::sync::mpsc::channel();
     let watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
-        Box::new(PollWatcher::with_delay(tx,Duration::from_secs(1)).unwrap())
+        Box::new(PollWatcher::with_delay(tx, Duration::from_secs(1)).unwrap())
     } else {
         Box::new(RecommendedWatcher::new(tx).unwrap())
     };
