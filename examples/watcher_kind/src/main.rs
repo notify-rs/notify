0245625999e728ed3ce8dc6fdd5b6a7ad17e6510
Jan Scheer <jhscheer@users.noreply.github.com>
2022-05-21 05:17:41 +0200

    examples: fix watcher_kind example to work with the changes from #396 (#407)

--- examples/watcher_kind/src/main.rs
+++ examples/watcher_kind/src/main.rs
@@ -1,10 +1,14 @@
 use std::time::Duration;
 
-use notify::*;
+use notify::{poll::PollWatcherConfig, *};
 fn main() {
-    let (tx, rx) = std::sync::mpsc::channel();
-    let watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
-        Box::new(PollWatcher::with_delay(tx, Duration::from_secs(1)).unwrap())
+    let (tx, _rx) = std::sync::mpsc::channel();
+    let _watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
+        let config = PollWatcherConfig {
+            poll_interval: Duration::from_secs(1),
+            ..Default::default()
+        };
+        Box::new(PollWatcher::with_config(tx, config).unwrap())
     } else {
         Box::new(RecommendedWatcher::new(tx).unwrap())
     };
