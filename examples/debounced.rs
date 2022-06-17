81583950fdf135608a2d7475cc3c06c35a523b1d
Aron Heinecke <aron.heinecke@t-online.de>
2022-06-17 22:00:24 +0200

    send errors in debouncer, add example, name thread

---
+++ examples/debounced.rs
@@ -0,0 +1,26 @@
+use std::{path::Path, time::Duration};
+
+use notify::{new_debouncer, RecursiveMode, Watcher};
+
+fn main() {
+    std::thread::spawn(|| {
+        let path = Path::new("test.txt");
+        let _ = std::fs::remove_file(&path);
+        loop {
+            std::fs::write(&path, b"Lorem ipsum").unwrap();
+            std::thread::sleep(Duration::from_millis(250));
+        }
+    });
+
+    let (rx, mut watcher) = new_debouncer(Duration::from_secs(2)).unwrap();
+
+    watcher
+        .watch(Path::new("."), RecursiveMode::Recursive)
+        .unwrap();
+
+    for events in rx {
+        for e in events {
+            println!("{:?}", e);
+        }
+    }
+}
