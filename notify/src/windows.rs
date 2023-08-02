4a0d3b7f9729cb7aec3022ed123954c476e098cf
kuzeyardabulut <54737933+kuzeyardabulut@users.noreply.github.com>
2023-08-02 19:41:06 +0300

    Fix Logic Error

--- notify/src/windows.rs
+++ notify/src/windows.rs
@@ -301,10 +301,13 @@ fn start_read(rd: &ReadData, event_handler: Arc<Mutex<dyn EventHandler>>, handle
 
         if ret == 0 {
             // error reading. retransmute request memory to allow drop.
-            // allow overlapped to drop by dropping ManuallyDrop
+            // allow overlapped to drop by omitting forget()
             let request: Box<ReadDirectoryRequest> = mem::transmute(request_p);
-            std::mem::ManuallyDrop::drop(&mut overlapped);
+
             ReleaseSemaphore(request.data.complete_sem, 1, ptr::null_mut());
+        } else {
+            // read ok. forget overlapped to let the completion routine handle memory
+            std::mem::ManuallyDrop::drop(&mut overlapped);
         }
     }
 }
