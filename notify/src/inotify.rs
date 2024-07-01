f6312896baeea1b4da615c2352ea275ad0c994b7
y5c4l3 <y5c4l3@proton.me>
2024-07-01 19:15:07 +0800

    Make `ErrorKind::PathNotFound` consistent on inotify
    Before this patch, watching a nonexistent path using inotify backend
    would give an `ErrorKind::Io` variant. This behavior was inconsistent
    with other backends.
    
    Signed-off-by: y5c4l3 <y5c4l3@proton.me>

--- notify/src/inotify.rs
+++ notify/src/inotify.rs
@@ -393,7 +393,7 @@ impl EventLoop {
     fn add_watch(&mut self, path: PathBuf, is_recursive: bool, mut watch_self: bool) -> Result<()> {
         // If the watch is not recursive, or if we determine (by stat'ing the path to get its
         // metadata) that the watched path is not a directory, add a single path watch.
-        if !is_recursive || !metadata(&path).map_err(Error::io)?.is_dir() {
+        if !is_recursive || !metadata(&path).map_err(Error::io_watch)?.is_dir() {
             return self.add_single_watch(path, false, true);
         }
 
