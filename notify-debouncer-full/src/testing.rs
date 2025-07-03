40f78faa06ab166ed1953d7870d331cee780ddc8
Yuki Okushi <huyuumi.dev@gmail.com>
2025-07-03 19:29:39 +0900

    chore: Fix clippy warning

--- notify-debouncer-full/src/testing.rs
+++ notify-debouncer-full/src/testing.rs
@@ -290,7 +290,7 @@ impl TestCache {
 }
 
 impl FileIdCache for TestCache {
-    fn cached_file_id(&self, path: &Path) -> Option<impl AsRef<FileId>> {
+    fn cached_file_id(&self, path: &Path) -> Option<&FileId> {
         self.paths.get(path)
     }
 
