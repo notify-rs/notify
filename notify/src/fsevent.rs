cc0acb8d5dc0ec2a68da286220d3b543600b7a1b
翠 <green@sapphi.red>
2026-01-13 21:15:58 +0900

    chore: remove license

--- notify/src/fsevent.rs
+++ notify/src/fsevent.rs
@@ -1077,36 +1077,6 @@ mod tests {
     // fsevents seems to not allow watching more than 4096 paths at once.
     // https://github.com/fsnotify/fsevents/issues/48
     // Based on https://github.com/fsnotify/fsevents/commit/3899270de121c963202e6fed46aa31d5ec7b3908
-    //
-    // ```
-    // Copyright © The fsevents Authors. All rights reserved.
-    //
-    // Redistribution and use in source and binary forms, with or without
-    // modification, are permitted provided that the following conditions are
-    // met:
-    //
-    //    * Redistributions of source code must retain the above copyright
-    // notice, this list of conditions and the following disclaimer.
-    //    * Redistributions in binary form must reproduce the above
-    // copyright notice, this list of conditions and the following disclaimer
-    // in the documentation and/or other materials provided with the
-    // distribution.
-    //    * Neither the name of Google Inc. nor the names of its
-    // contributors may be used to endorse or promote products derived from
-    // this software without specific prior written permission.
-    //
-    // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-    // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-    // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    // SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-    // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-    // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    // ```
     #[test]
     fn error_properly_on_stream_start_failure() {
         let tmpdir = testdir();
